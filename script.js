398187e154de13ff150831608bc1960363cf78f3
Ruslan Lopushan <tguruslan1994@gmail.com>
2024-12-02 14:41:42 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ if (typeof navigator.serviceWorker !== 'undefined') {
 }
 const URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vT_jDlK16raPFNfxz-rvNX9DQYDT0LnNTOGJAa5DqFJlMnnkZyr8Gutgb8yciiXy10a2sordGS8DPe4/pubhtml?gid=0&single=true';
 
-let skip = ["набирає"];
+let skip = ["набирає","вихідний"];
 
 function filterData(data, skip){
   const resp = [];

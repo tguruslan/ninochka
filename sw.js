9db96f867e54745917a7d81b7c71abe4cbb38124
Ruslan Lopushan <tguruslan1994@gmail.com>
2025-01-27 17:51:21 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -8,6 +8,26 @@ self.addEventListener("message", (event) => {
   }
 });
 
+Notification.requestPermission().then((result) => {
+    if (result === "granted") {
+      setTimeout(function(){
+          const now = new Date();
+          const now_ts = now.getTime();
+          $("[time]").each(function(){
+              diff_time = $(this).attr("time") - now_ts
+              if(diff_time > 0 && diff_time < (35 * 60 * 1000)){
+                  const options = {
+                    body: $(this).text()
+                  }
+                  alert($(this).text());
+                  new Notification("rozklad", options);
+                  $(this).removeAttr("time");
+              }
+            });
+        }, 5000);
+    }
+});
+
 workbox.routing.registerRoute(
   new RegExp('/*'),
   new workbox.strategies.StaleWhileRevalidate({

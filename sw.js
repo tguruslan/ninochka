ace18db95b43041c023db7d3bf2c394414099a0b
Ruslan Lopushan <tguruslan1994@gmail.com>
2025-01-28 15:45:04 +0200

    2

--- sw.js
+++ sw.js
@@ -15,30 +15,10 @@ workbox.routing.registerRoute(
   })
 );
 
-function setCookie(name,value,days) {
-  var expires = "";
-  if (days) {
-      var date = new Date();
-      date.setTime(date.getTime() + (days*24*60*60*1000));
-      expires = "; expires=" + date.toUTCString();
-  }
-  document.cookie = name + "=" + (value || "")  + expires + "; path=/";
-}
-function getCookie(name) {
-  var nameEQ = name + "=";
-  var ca = document.cookie.split(';');
-  for(var i=0;i < ca.length;i++) {
-      var c = ca[i];
-      while (c.charAt(0)==' ') c = c.substring(1,c.length);
-      if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
-  }
-  return null;
-}
-
-if (! getCookie("notification")){
+if (! self.cookies.get("notification")){
     Notification.requestPermission().then((result) => {
         if (result === "granted") {
-          setCookie("notification","1",700);
+          self.cookies.set("notification","1");
         }
     });
 }
